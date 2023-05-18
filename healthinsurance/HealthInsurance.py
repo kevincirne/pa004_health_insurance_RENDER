14484e323b7521e4e9c5638b51c6c011ad5979e6
Kevin Cirne <kevincleal@gmail.com>
2023-05-18 13:03:20 -0300

    Handler.py Correction

--- healthinsurance/HealthInsurance.py
+++ healthinsurance/HealthInsurance.py
@@ -13,7 +13,7 @@ class HealthInsurance:
         self.target_encoder_region_code_scaler =                     pickle.load(open(self.home_path + 'paramater/target_encoder_region_code_scaler.pkl', 'rb'))
         self.fe_policy_sales_channel_scaler =                        pickle.load(open(self.home_path + 'paramater/fe_policy_sales_channel_scaler.pkl', 'rb'))       
         
-    def data_cleaning(data):
+    def data_cleaning(self, data):
         cols_new = ['id', 'gender', 'age', 'driving_license', 'region_code', 'previously_insured', 'vehicle_age', 'vehicle_damage', 'annual_premium',
        'policy_sales_channel', 'vintage', 'response']
         
@@ -21,14 +21,14 @@ class HealthInsurance:
         
         return data
     
-    def feature_engineering(data):
+    def feature_engineering(self, data):
         data['vehicle_age'] = data['vehicle_age'].apply(lambda x: 'over_2_years' if x == '> 2 Years' else 'between_1_2_years' if x == '1-2 Year' else 'below_1_year')
 
         data['vehicle_damage'] = data['vehicle_damage'].apply(lambda x: 1 if x == 'Yes' else 0)
         
         return data
         
-    def data_preparation (data):
+    def data_preparation (self, data):
         # annual_premium
         data['annual_premium'] = self.annual_premium_scaler.transform(data[['annual_premium']].values)
         
@@ -61,6 +61,6 @@ class HealthInsurance:
         pred = model.predict_proba(test_data)
         
         #Join prediction to original data
-        original_data['prediction'] = pred
+        original_data['score'] = pred[:, 1].tolist()
         
         return original_data.to_json(orient = 'records', date_format='iso')
